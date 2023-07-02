e7a4f54471dabf0498fd62cae6b49674e91245cf
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-07-02 18:55:00 +0700

    done main menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public string firstLevelName;
+    [SerializeField] private Slider volumeSlider;
+
+    public void Start()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("Volume", 0.5f);
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
 
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync(firstLevelName);
+        SceneManager.LoadSceneAsync("Main");
     }
 
     public void QuitGame()
@@ -17,5 +25,16 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
 
         Debug.Log("Quit");
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void SaveSettings()
+    {
+        float volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("Volume", volume);
     }
 }
